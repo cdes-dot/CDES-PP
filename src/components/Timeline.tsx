df8d50d0f9ed250c5a521201b10a80692cd7cf89
Nelcido Rafael Diaz Delgado <nelcidodiaz@Nelcidos-MacBook-Air.local>
2025-08-05 10:27:02 -0400

    Fixing timeline on about us page

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -9,40 +9,46 @@ type TimelineEntry = {
 
 const Timeline = ({ timeline }: { timeline: any }) => {
   const [currentEvent, setCurrentEvent] = useState(timeline[0]);
+  
   return (
     <section className="py-32">
-      <div className="container">
+      <div className="gap-4">
         <h1 className="text-foreground mb-10 text-center text-3xl font-bold tracking-tighter ">
           Nuestra Historia
         </h1>
-        <div className="flex lg:flex-row flex-col relative mx-auto max-w-4xl gap-4">
-          <div className="w-4/5">
+        <div className="flex lg:flex-row flex-col relative w-full space-x-6">
+
+          <div className="lg:w-4/5 w-full">
             <h1 className="text-foreground mb-10 text-center text-3xl font-bold tracking-tighter">
               {currentEvent.Titulo}
             </h1>
+            {
+              currentEvent.Media && (<img
+                className="rounded-md object-cover mb-10"
+                src={currentEvent.Media.url}
+              />)
+            }
+
             <p className="text-muted-foreground mb-10 text-center text-lg tracking-tight">
               {currentEvent.Descripcion}
             </p>
           </div>
-          <div className="w-1/5">
+          <div className="lg:w-1/2 w-full">
             {timeline.map((entry: any, index: number) => (
-              <Card className="my-5 border-none shadow-none relative mb-10 pl-8" onClick={() => setCurrentEvent(entry)}>
+              <Card className="border-none relative mb-10 pl-8 cursor-pointer h-fit shadow-none bg-neutral-200 " onClick={() => setCurrentEvent(entry)}>
                 <CardHeader>
-                  <CardTitle className="rounded-xl py-2 text-xl font-bold tracking-tight xl:mb-4 xl:px-3">
+                  <CardTitle className="rounded-xl text-lg font-bold tracking-tight">
                     {entry.Titulo}
                   </CardTitle>
-
-                  <CardDescription className="text-md -left-34 text-muted-foreground top-3 rounded-xl tracking-tight xl:absolute">
-                    {entry.Fecha}
+                  <CardDescription className="text-muted-foreground text-sm">
+                    {new Date(entry.Fecha).toLocaleDateString("es-ES", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
                   </CardDescription>
-
                 </CardHeader>
-                <CardContent className="px-0 xl:px-2">
-                  <div
-                    className="prose dark:prose-invert text-foreground mx-2"
-                    dangerouslySetInnerHTML={{ __html: entry.Descripcion }}
-                  />
-                </CardContent>
+
               </Card>
             ))}
           </div>
