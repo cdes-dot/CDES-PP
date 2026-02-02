a0d9d2e123ff1f208726d67efd0096375bbe919f
Erick De la Rosa <eddv0001@ce.pucmm.edu.do>
2026-02-02 15:08:20 -0400

    Probando

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,19 +1,17 @@
 export const getData = async (url: string, meta: boolean = false) => {
   try {
-    const fullUrl = `${import.meta.env.PUBLIC_STRAPI_URL}/api/${url}`;
-    console.log("Fetching URL:", fullUrl);
-    
-    const response = await fetch(fullUrl, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${import.meta.env.PUBLIC_STRAPI_KEY}`,
+    const response = await fetch(
+      `${import.meta.env.PUBLIC_STRAPI_URL}/api/${url}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${import.meta.env.PUBLIC_STRAPI_KEY}`,
+        },
       },
-    });
+    );
 
     if (!response.ok) {
-      const errorBody = await response.text();
-      console.error("Error response body:", errorBody);
-      throw new Error(`HTTP error! status: ${response.status} - ${errorBody}`);
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     if (meta) {
